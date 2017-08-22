4d78251473c9d424eacaf41feaa5399009a97698
eason <dontsaygoodbyebecause520@gmail.com>
2017-08-22 17:52:43 +0800

    "seeder_and_model"

--- seeder.go
+++ seeder.go
@@ -10,7 +10,7 @@ var db gorm.DB
 
 func main() {
 
-	db, err := gorm.Open("mysql", "root:ab789789@/goblog?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("mysql", "root:password@/goblog?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		return
 	}
